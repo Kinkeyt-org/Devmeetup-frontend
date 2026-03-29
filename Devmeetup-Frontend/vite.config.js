781567bee445f2ccdf2989135f3c179a81355577
OKOYE <emekao9376@gmail.com>
2026-03-29 22:12:10 +0100

    Security added

--- Devmeetup-Frontend/vite.config.js
+++ Devmeetup-Frontend/vite.config.js
@@ -7,8 +7,4 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-
-  server:{
-    historyApiFallback: true, // Enable history API fallback for SPA routing. This prevents 404 errors when refreshing or directly accessing routes in a React Router application. It ensures that all requests are served the index.html file, allowing the client-side router to handle routing correctly.
-  }
 })
