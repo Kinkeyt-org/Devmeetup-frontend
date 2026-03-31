e551a3d6bed78ef18f34e945773059ac82a71cbd
OKOYE <emekao9376@gmail.com>
2026-03-31 22:53:24 +0100

    events done

--- Devmeetup-Frontend/src/pages/Home.jsx
+++ Devmeetup-Frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import CreatePage from './CreatePage'
+
 
 const Home = () => {
   return (
     <div className='mx-auto w-full '>
         <Navbar/>
-        <CreatePage/>
+        
     </div>
   )
 }
