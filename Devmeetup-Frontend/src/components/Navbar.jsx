890491453a87380f156239b386dcd8bebaf57efb
OKOYE <emekao9376@gmail.com>
2026-04-01 20:24:08 +0100

    corrections

--- Devmeetup-Frontend/src/components/Navbar.jsx
+++ Devmeetup-Frontend/src/components/Navbar.jsx
@@ -79,9 +79,9 @@ const Navbar = () => {
               </div>
               <button 
                 onClick={() => setIsSearchOpen(false)} 
-                className="text-sm font-black text-gray-500 p-2 hover:bg-gray-50 rounded-xl transition-colors"
+                className="text-sm font-black cursor-pointer text-gray-500 p-2 hover:bg-gray-50 rounded-xl transition-colors"
               >
-                CANCEL
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
               </button>
             </motion.div>
           )}
