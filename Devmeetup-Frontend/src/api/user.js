3dbb590542e2c60b69ba0f558c3bade09072349a
OKOYE <emekao9376@gmail.com>
2026-04-06 23:04:52 +0100

    new

--- Devmeetup-Frontend/src/api/user.js
+++ Devmeetup-Frontend/src/api/user.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_BASE_URL || "https://devmeetup.duckdns.org/api";
+const API_URL = import.meta.env.VITE_API_BASE_URL || "https://devmeetup.duckdns.org";
 
 const api = axios.create({
   baseURL: API_URL,
