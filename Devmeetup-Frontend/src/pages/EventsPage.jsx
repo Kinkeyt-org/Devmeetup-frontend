9c7fd393054488172376fffa6d61c855bcb7a1dc
OKOYE <emekao9376@gmail.com>
2026-04-08 20:13:29 +0100

    update

--- Devmeetup-Frontend/src/pages/EventsPage.jsx
+++ Devmeetup-Frontend/src/pages/EventsPage.jsx
@@ -100,7 +100,7 @@ const ExploreEvents = () => {
                       className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-700"
                     />
 
-                    <div className="absolute inset-0 bg-linear-to-t from-black/60 via-transparent to-transparent opacity-60 group-hover:opacity-80 transition-opacity" />
+                    <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent transition-opacity group-hover:opacity-90" />
 
                     <div className="absolute bottom-3 left-6 text-white">
                       <h3 className="text-xl font-bold leading-tight tracking-tight">
