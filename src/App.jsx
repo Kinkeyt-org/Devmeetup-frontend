7891a0ed521142ec4e1f8d4259461843ccfcde91
OKOYE <emekao9376@gmail.com>
2026-06-28 20:45:08 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,7 @@ import EventsPage from "./pages/EventsPage";
 import EventDetails from "./pages/EventDetails";
 import Upgrade from "./pages/Upgrade";
 import OrganizerOnboarding from "./pages/OrganizerOnboarding";
-import Tech from "./pages/categories/Tech";
-import Design from "./pages/categories/Design";
-import Business from "./pages/categories/Business";
-import Lifestyle from "./pages/categories/Lifestyle";
-import Education from "./pages/categories/Education";
-import Health from "./pages/categories/Health";
-import Music from "./pages/categories/Music";
-import Social from "./pages/categories/Social";
+import CategoryPage from "./pages/categories/CategoryPage";
 import PersonalInfo from "./pages/settings/PersonalInfo";
 import Security from "./pages/settings/Security";
 import Payments from "./pages/settings/Payments";
@@ -168,14 +161,7 @@ const AppContent = () => {
           <Route path="/search" element={<Search />} />
           <Route path="/events/create" element={<CreatePage />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/categories/tech" element={<Tech />} />
-          <Route path="/categories/design" element={<Design />} />
-          <Route path="/categories/business" element={<Business />} />
-          <Route path="/categories/lifestyle" element={<Lifestyle />} />
-          <Route path="/categories/education" element={<Education />} />
-          <Route path="/categories/health" element={<Health />} />
-          <Route path="/categories/music" element={<Music />} />
-          <Route path="/categories/social" element={<Social />} />
+          <Route path="/categories/:category" element={<CategoryPage />} />
           <Route path="/my-tickets" element={<MyTickets />} />
           <Route path="/events/:id" element={<EventDetails />} />
           <Route path="/upgrade" element={<Upgrade />} />
