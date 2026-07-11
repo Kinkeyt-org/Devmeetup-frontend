24fa2eccff54aeb834287b51ab84975c1863951a
OKOYE <emekao9376@gmail.com>
2026-07-11 21:34:26 +0100

    explore and profile.jsx edited

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Explore = () => (
-  <div className="flex items-center justify-center  dark:bg-neutral-950">
-    <h1 className="text-xl font-bold text-neutral-950 dark:text-neutral-50 animate-pulse">
+  <div className="flex items-center justify-center min-h-screen dark:bg-neutral-950">
+    <h1 className="text-2xl font-bold text-neutral-950 dark:text-neutral-50 animate-pulse">
       Coming Soon
     </h1>
   </div>
