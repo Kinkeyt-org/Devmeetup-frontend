bd4d6b94605e3a689d6c99ac49678a11ce2363a1
OKOYE <emekao9376@gmail.com>
2026-07-11 11:23:17 +0100

    changes to navbar and slider

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Explore = () => (
-  <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-indigo-100 via-purple-50 to-pink-100 dark:from-gray-800 dark:to-gray-900">
-    <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-100">
+  <div className="flex items-center justify-center min-h-screen dark:bg-neutral-950">
+    <h1 className="text-2xl font-bold text-neutral-950 dark:text-neutral-50">
       Coming Soon
     </h1>
   </div>
